69b9437dfd6f6028d47901fa0c2234f6b7f1d5b2
Ram Gochade <ramgochade66@gmail.com>
2026-03-14 10:48:00 +0530

    Create Pattern

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+class Pattern{
+public void printPattern(){
+  int num=1;
+
+  for(int i=1;i<=15;i++)
+  {
+      for(int j=1;j<=i && num<=15;j++)
+      {
+          System.out.print(num +" ");
+          num++;
+      }
+      system.out.println();
+  }
+
+}
+}
